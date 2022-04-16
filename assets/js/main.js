3d6c871acbce2bf81e6ea210502a148f95fff5f5
dillahmwangi <mcharity241@gmail.com>
2022-04-16 14:15:39 +0300

    All declarations made

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,26 @@
-let name1 = document.getElementById("name");
-let button = document.getElementById("btn");
-let colors=["red", "blue", "green"];
-const changeColor = () => {
-    document.body.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)]
-}
+const clearButton = document.getElementById("clearButton");
+const leftBracketButton = document.getElementById("leftBracketButton");
+const rightBracketButton = document.getElementById("rightBracketButton");
+const multiplicationButton = document.getElementById("multiplicationButton");
+const squareRootButton = document.getElementById("squareRootButton");
+const percentageButton = document.getElementById("percentageButton");
+const plusMinusButton = document.getElementById("plusMinusButton");
+const divideButton = document.getElementById("divideButton");
+const sevenButton = document.getElementById("sevenButton");
+const eightButton = document.getElementById("eightButton");
+const nineButton = document.getElementById("nineButton");
+const minusButton = document.getElementById("minusButton");
+const fourButton = document.getElementById("fourButton");
+const fiveButton = document.getElementById("fiveButton");
+const sixButton = document.getElementById("sixButton");
+const plusButton = document.getElementById("plusButton");
+const oneButton = document.getElementById("oneButton");
+const twoButton = document.getElementById("twoButton");
+const threeButton = document.getElementById("threeButton");
+const decimalButton = document.getElementById("decimalButton");
+const zeroButton = document.getElementById("zeroButton");
+const deleteButton = document.getElementById("deleteButton");
+const equalsButton = document.getElementById("equalsButton");
 
-Math.random();
 
-console.log(name1);
-
-button.addEventListener('click', () => changeColor());
 
